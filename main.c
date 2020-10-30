c7dc0efc9e7e5721b9e3041f7d59ffd3d90797ce
ViktorVlasov <vlasov_viktor01@mail.ru>
2020-10-30 17:14:29 +0300

    all done

--- main.c
+++ main.c
@@ -6,19 +6,9 @@
 /*   By: efumiko <efumiko@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/30 15:48:42 by efumiko           #+#    #+#             */
-/*   Updated: 2020/10/30 17:07:27 by efumiko          ###   ########.fr       */
+/*   Updated: 2020/10/30 17:13:47 by efumiko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-
-# include <stddef.h>
-# include <errno.h>
-# include <stdlib.h>
-# include <string.h>
-# include <fcntl.h>
-# include <stdio.h>
-# include <unistd.h>
-
 
 #include "libasm.h"
 
@@ -34,88 +24,88 @@ int main(void)
     ***********
     */
 
-	// printf("\n----READ_TEST----\n");
-	// buf1 = (char*)calloc(255, sizeof(char));
-	// buf2 = (char*)calloc(255, sizeof(char));
-	// printf("STDIN CORRECT; read: %li ---- ft_read: %li\nstring: %s", read(0, buf1, 10), ft_read(0, buf2, 10), buf1);
-	// printf("errno: %i\n\n", errno);
-	// bzero(buf1, 255);
-	// bzero(buf2, 255);
+	printf("\n----READ_TEST----\n");
+	buf1 = (char*)calloc(255, sizeof(char));
+	buf2 = (char*)calloc(255, sizeof(char));
+	printf("STDIN CORRECT; read: %li ---- ft_read: %li\nstring: %s", read(0, buf1, 10), ft_read(0, buf2, 10), buf1);
+	printf("errno: %i\n\n", errno);
+	bzero(buf1, 255);
+	bzero(buf2, 255);
 
-	// printf("STDIN UNCORRECT; read: %li ---- ft_read: %li\n", read(0, buf1, -10), ft_read(0, buf2, -10));
-	// printf("errno: %i\n\n", errno);
-	// bzero(buf1, 255);
-	// bzero(buf2, 255);
+	printf("STDIN UNCORRECT; read: %li ---- ft_read: %li\n", read(0, buf1, -10), ft_read(0, buf2, -10));
+	printf("errno: %i\n\n", errno);
+	bzero(buf1, 255);
+	bzero(buf2, 255);
 
-    // errno = 0;
-    // int		fd1;
-    // int     fd2;
-    // fd1 = open("./ft_read.s", O_RDWR);
-    // fd2 = open("./ft_read.s", O_RDWR);
-	// printf("FROM FILE; read: %li ---- ft_read: %li\n", ft_read(fd1, buf1, 255), ft_read(fd2, buf2, 255));
-    // printf("errno: %i\n\n", errno);
-    // bzero(buf1, 255);
-	// bzero(buf2, 255);
-    // close(fd1);
-    // close(fd2);
+    errno = 0;
+    int		fd1;
+    int     fd2;
+    fd1 = open("./ft_read.s", O_RDWR);
+    fd2 = open("./ft_read.s", O_RDWR);
+	printf("FROM FILE; read: %li ---- ft_read: %li\n", ft_read(fd1, buf1, 255), ft_read(fd2, buf2, 255));
+    printf("errno: %i\n\n", errno);
+    bzero(buf1, 255);
+	bzero(buf2, 255);
+    close(fd1);
+    close(fd2);
 
-    // fd1 = open("./ft_read.s", O_WRONLY);
-    // fd2 = open("./ft_read.s", O_WRONLY);
-	// printf("FROM FILE (ONLY WRITE); read: %li ---- ft_read: %li\n", ft_read(fd1, buf1, 255), ft_read(fd2, buf2, 255));
-	// printf("errno: %i\n\n", errno);
-	// bzero(buf1, 255);
-	// bzero(buf2, 255);
-    // close(fd1);
-    // close(fd2);
+    fd1 = open("./ft_read.s", O_WRONLY);
+    fd2 = open("./ft_read.s", O_WRONLY);
+	printf("FROM FILE (ONLY WRITE); read: %li ---- ft_read: %li\n", ft_read(fd1, buf1, 255), ft_read(fd2, buf2, 255));
+	printf("errno: %i\n\n", errno);
+	bzero(buf1, 255);
+	bzero(buf2, 255);
+    close(fd1);
+    close(fd2);
     
-    // printf("UNCORRET FD; read: %li ---- ft_read: %li\n", ft_read(74, buf1, 255), ft_read(74, buf2, 255));
-    // printf("errno: %i\n\n", errno);
-    // bzero(buf1, 255);
-	// bzero(buf2, 255);
+    printf("UNCORRET FD; read: %li ---- ft_read: %li\n", ft_read(74, buf1, 255), ft_read(74, buf2, 255));
+    printf("errno: %i\n\n", errno);
+    bzero(buf1, 255);
+	bzero(buf2, 255);
 
-    // free(buf1);
-    // free(buf2);
+    free(buf1);
+    free(buf2);
     
     /* 
     ************
     * FT_WRITE *
     ************
     */
-    // int w1;
-    // int w2;
+    int w1;
+    int w2;
 
-    // errno = 0;
-    // printf("\n----WRITE_TEST----\n");
+    errno = 0;
+    printf("\n----WRITE_TEST----\n");
     
-    // printf("\n**************************\n***TEST count > str_len***\n**************************\n");
-    // w1 = write(1, "Hello, world!", 14);
-    // printf("\nwrite ret: %d errno: %i\n", w1, errno);
-    // w2 = ft_write(1, "Hello, world!", 14);
-	// printf("\nft_write ret: %d errno: %i\n", w2, errno);
+    printf("\n**************************\n***TEST count > str_len***\n**************************\n");
+    w1 = write(1, "Hello, world!", 14);
+    printf("\nwrite ret: %d errno: %i\n", w1, errno);
+    w2 = ft_write(1, "Hello, world!", 14);
+	printf("\nft_write ret: %d errno: %i\n", w2, errno);
 	
-    // printf("\n\n**************************\n***TEST count < str_len***\n**************************\n");
-    // w1 = write(1, "Hello, world!\n", 3);
-    // printf("\nwrite ret: %d errno: %i\n", w1, errno);
-    // w2 = ft_write(1, "Hello, world!\n", 3);
-	// printf("\nft_write ret: %d errno: %i\n", w2, errno);
+    printf("\n\n**************************\n***TEST count < str_len***\n**************************\n");
+    w1 = write(1, "Hello, world!\n", 3);
+    printf("\nwrite ret: %d errno: %i\n", w1, errno);
+    w2 = ft_write(1, "Hello, world!\n", 3);
+	printf("\nft_write ret: %d errno: %i\n", w2, errno);
 	
-    // printf("\n*********************\n***TEST count == 0***\n*********************");
-    // w1 = write(1, "Hello, world!\n", 0);
-    // printf("\nwrite ret: %d errno: %i", w1, errno);
-    // w2 = ft_write(1, "Hello, world!\n", 0);
-	// printf("\nft_write ret: %d errno: %i\n", w2, errno);
+    printf("\n*********************\n***TEST count == 0***\n*********************");
+    w1 = write(1, "Hello, world!\n", 0);
+    printf("\nwrite ret: %d errno: %i", w1, errno);
+    w2 = ft_write(1, "Hello, world!\n", 0);
+	printf("\nft_write ret: %d errno: %i\n", w2, errno);
 
-    // printf("\n********************\n***TEST count < 0***\n********************");
-    // w1 = write(1, "Hello, world!\n", -10);
-    // printf("\nwrite ret: %d errno: %i", w1, errno);
-    // w2 = ft_write(1, "Hello, world!\n", -10);
-	// printf("\nft_write ret: %d errno: %i\n", w2, errno);
+    printf("\n********************\n***TEST count < 0***\n********************");
+    w1 = write(1, "Hello, world!\n", -10);
+    printf("\nwrite ret: %d errno: %i", w1, errno);
+    w2 = ft_write(1, "Hello, world!\n", -10);
+	printf("\nft_write ret: %d errno: %i\n", w2, errno);
 
-    // printf("\n***********************\n***TEST UNCORRECT FD***\n***********************");
-    // w1 = write(74, "Hello, world!\n", 10);
-    // printf("\nwrite ret: %d errno: %i", w1, errno);
-    // w2 = ft_write(74, "Hello, world!\n", 10);
-	// printf("\nft_write ret: %d errno: %i\n", w2, errno);
+    printf("\n***********************\n***TEST UNCORRECT FD***\n***********************");
+    w1 = write(74, "Hello, world!\n", 10);
+    printf("\nwrite ret: %d errno: %i", w1, errno);
+    w2 = ft_write(74, "Hello, world!\n", 10);
+	printf("\nft_write ret: %d errno: %i\n", w2, errno);
 
     
     /* 
@@ -124,12 +114,12 @@ int main(void)
     ************
     */
 
-    // printf("\n----STRLEN_TEST----\n");
-	// printf("%li\n", ft_strlen("fshdjfjdsjfdskf8eru8239829d3824y32y432y4983284e238eh238984e2384e8239923d23849823949234"));
-	// printf("%li\n", ft_strlen("a"));
-	// printf("%li\n", ft_strlen("ab"));
-	// printf("%li\n", ft_strlen("abc"));
-	// printf("%li\n", ft_strlen("abcd"));
+    printf("\n----STRLEN_TEST----\n");
+	printf("%li\n", ft_strlen("fshdjfjdsjfdskf8eru8239829d3824y32y432y4983284e238eh238984e2384e8239923d23849823949234"));
+	printf("%li\n", ft_strlen("a"));
+	printf("%li\n", ft_strlen("ab"));
+	printf("%li\n", ft_strlen("abc"));
+	printf("%li\n", ft_strlen("abcd"));
     
     
     /* 
@@ -138,15 +128,15 @@ int main(void)
     ************
     */
 
-    // printf("\n----STRCMP_TEST----\n");
-	// printf("s1 == s2: %d\n", ft_strcmp("abcd", "abcd"));
-	// printf("s1 == s2: %d\n", ft_strcmp("", ""));
-	// printf("s1 < s2 : %d\n", ft_strcmp("a", "abc"));
-	// printf("s1 < s2 : %d\n", ft_strcmp("", "abc"));
-	// printf("s1 < s2 : %d\n", ft_strcmp("123", "123abc"));
-	// printf("s1 > s2 : %d\n", ft_strcmp("abc", "a"));
-	// printf("s1 > s2 : %d\n", ft_strcmp("abc", ""));
-	// printf("s1 > s2 : %d\n", ft_strcmp("123abc", "123"));
+    printf("\n----STRCMP_TEST----\n");
+	printf("s1 == s2: %d\n", ft_strcmp("abcd", "abcd"));
+	printf("s1 == s2: %d\n", ft_strcmp("", ""));
+	printf("s1 < s2 : %d\n", ft_strcmp("a", "abc"));
+	printf("s1 < s2 : %d\n", ft_strcmp("", "abc"));
+	printf("s1 < s2 : %d\n", ft_strcmp("123", "123abc"));
+	printf("s1 > s2 : %d\n", ft_strcmp("abc", "a"));
+	printf("s1 > s2 : %d\n", ft_strcmp("abc", ""));
+	printf("s1 > s2 : %d\n", ft_strcmp("123abc", "123"));
 
     
     /* 
@@ -155,15 +145,15 @@ int main(void)
     ************
     */
 
-    // char *str;
+    char *s;
 
-	// printf("\n----STRCMP_TEST----\n");
-	// str = (char*)malloc(sizeof(char) * 30);
-	// printf("%s\n", ft_strcpy(str, ""));
-	// printf("%s\n", ft_strcpy(str, "123"));
-	// printf("%s\n", ft_strcpy(str, "1234"));
-    // printf("%s\n", ft_strcpy(str, "asfsdhfjdsfdsfsdhjfsdhkgsdgfdsfdsfsdfsdgdhsfdfdsghjkjhgfdfghjkjhgfdghjkd"));
-	// free(str);
+	printf("\n----STRCPY_TEST----\n");
+	s = (char*)malloc(sizeof(char) * 30);
+	printf("%s\n", ft_strcpy(s, ""));
+	printf("%s\n", ft_strcpy(s, "123"));
+	printf("%s\n", ft_strcpy(s, "1234"));
+    printf("%s\n", ft_strcpy(s, "asfsdhfjdsfdsfsdhjfsdhkgsdgfdsfdsfsdfsdgdhsfdfdsghjkjhgfdfghjkjhgfdghjkd"));
+	free(s);
     
 
     /* 
